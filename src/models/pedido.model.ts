d8f2c61edfcaeeeffef3fa2fe82e3ecd831ca861
Steven <estiventrejos13@hotmail.com>
2021-11-23 12:28:43 -0500

    prueba de git

--- src/models/pedido.model.ts
+++ src/models/pedido.model.ts
@@ -1,4 +1,4 @@
-import {belongsTo, Entity, model, property, hasOne} from '@loopback/repository';
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
 import {Persona} from './persona.model';
 import {Producto} from './producto.model';
 
